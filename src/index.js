1160dd298963c57b50a33a2cf665b454201556e4
va-z <vasilizadorojnyuk@gmail.com>
2020-09-15 18:53:33 +0500

    feat: solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const map = new Map(bracketsConfig);
+    const stack = [];
+
+    for (let char of str) {
+        // same principle, but w/ a useless optimization:
+        //
+        // if (!map.has(char) && char !== map.get(stack[stack.length - 1]))
+        //     return false;
+        // else if (char === map.get(stack[stack.length - 1])) stack.pop();
+        // else stack.push(char);
+
+        if (char === map.get(stack[stack.length - 1])) stack.pop();
+        else stack.push(char);
+    }
+
+    return !stack.length;
+};
